28b5e157bb29048906e16d5708010aa8be998f70
Zach <zmriel@gmail.com>
2021-04-14 20:54:36 -0400

    changed BaseType to be a plain object over an indexed one

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,6 @@ import { DEFAULT_COMPONENTS } from "./components/const";
 
 export type GridComponents = typeof DEFAULT_COMPONENTS;
 
-export type BaseType = Record<string, unknown>;
+export type BaseType = {};
 
 export type IdType = { id: number } & BaseType;
