47138d397a645f133c3fe5f29aeffcc5674b562c
Zach <zmriel@gmail.com>
2021-04-28 16:48:32 -0400

    types changes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import Grid from "./GridComponent";
 export * from "./GridComponent";
 export * from "./hooks";
 export * from "./types";
-export * from "./react-table-config";
 // re-export types from react-table to use in projects
 export * as reactTable from "react-table";
 
