0ca0eda126bfe544c411a693ea329aacd6ef35f7
Zach <zmriel@gmail.com>
2021-05-05 16:37:10 -0400

    table config moved to be included in exported type definitions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
+import "../react-table-config";
 import Grid from "./GridComponent";
-// import "./react-table-config";
 
 export * from "./GridComponent";
 export * from "./hooks";
 export * from "./types";
 // re-export types from react-table to use in projects
-export * from "./react-table-config";
 export * from "react-table";
 
 export default Grid;
