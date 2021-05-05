a1ca9fc308d7ca97f2ba144ca5ba6e386a61d7f8
Zach <zmriel@gmail.com>
2021-05-05 11:22:13 -0400

    changes to exported types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Grid from "./GridComponent";
-import "./react-table-config";
+// import "./react-table-config";
 
 export * from "./GridComponent";
 export * from "./hooks";
 export * from "./types";
 // re-export types from react-table to use in projects
+export * from "./react-table-config";
 export * from "react-table";
 
 export default Grid;
