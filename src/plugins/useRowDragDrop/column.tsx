9044b0cbcb2fed9e003463da29e20bffd76813b7
Zach <zmriel@gmail.com>
2022-04-18 20:26:08 -0400

    update deps

--- src/plugins/useRowDragDrop/column.tsx
+++ src/plugins/useRowDragDrop/column.tsx
@@ -1,16 +1,19 @@
-import DragHandle, { DragHandleProps } from "./DragHandle";
+import { type Column } from "react-table";
+import DragHandle from "./DragHandle";
 
 export const DRAG_HANDLE_COLUMN_ID = "drag-handle";
+
+// interface DragCellProps {
+//   dragHandleProps: DragHandleProps;
+// }
 
 /**
  * Drag handle columns to be used in drag and drop
  */
-export const dragHandleColumn = {
+export const dragHandleColumn: Column = {
   id: DRAG_HANDLE_COLUMN_ID,
   Header: "",
-  Cell: ({ dragHandleProps }: { dragHandleProps: DragHandleProps }) => (
-    <DragHandle {...dragHandleProps} />
-  ),
+  Cell: ({ dragHandleProps }: any) => <DragHandle {...dragHandleProps} />,
   disableSortBy: true,
   width: 25,
 };
