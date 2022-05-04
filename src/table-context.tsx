9e860c19bc224dcb2da9e4cae04afabe0ee0324a
Zach <zmriel@gmail.com>
2022-05-04 19:00:27 -0400

    chore: allow `any` in context declaration

--- src/table-context.tsx
+++ src/table-context.tsx
@@ -2,6 +2,7 @@ import { createContext, useContext } from "react";
 import type { TableInstance } from "react-table";
 import type { BaseType } from "./types";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const TableContext = createContext<TableInstance<any> | null>(null);
 TableContext.displayName = "TableContext";
 
