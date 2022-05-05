b38765970bb47fe4ba0fd1408774779497e4bd5f
Zach <zmriel@gmail.com>
2022-05-05 14:07:44 -0400

    fix(tests): ignore dist dir

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
 };
