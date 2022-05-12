4a957006cb762c61c1954fb7c4eae3937704f470
Zach <zmriel@gmail.com>
2022-05-12 10:50:01 -0400

    fix: turn off eslint type-checking rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
       plugins: ["@typescript-eslint"],
       extends: [
         "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        // "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
     },
   ],
