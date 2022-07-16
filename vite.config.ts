7c9e5ad61cb0d67fa087cb8e7c9083c9a314a375
Zach <zmriel@gmail.com>
2022-07-16 00:06:16 -0400

    chore: tweak vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-dts";
 
 export default defineConfig({
+  plugins: [dts()],
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
   build: {
     lib: {
-      entry: resolve("src/index.ts"),
+      entry: resolve(__dirname, "src/index.ts"),
       name: "MaterialGrid",
+      formats: ["es", "cjs"],
       fileName: "material-grid",
     },
     rollupOptions: {
-      // external: [
-      //   "@emotion/react",
-      //   "@emotion/styled",
-      //   "@mui/icons-material",
-      //   "@mui/material",
-      //   "react-dom",
-      //   "react",
-      // ],
+      external: [
+        "@emotion/react",
+        "@emotion/styled",
+        "@mui/icons-material",
+        "@mui/material",
+        "react-dom",
+        "react",
+      ],
       output: {
         globals: {
           react: "React",
