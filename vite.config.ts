c62991f1a48909c11f76168ac6f5063254df2f06
Zach <zmriel@gmail.com>
2022-07-16 02:01:02 -0400

    chore: more vite tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import dts from "vite-dts";
 
 export default defineConfig({
-  plugins: [dts()],
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
@@ -15,19 +14,21 @@ export default defineConfig({
       fileName: "material-grid",
     },
     rollupOptions: {
-      external: [
-        "@emotion/react",
-        "@emotion/styled",
-        "@mui/icons-material",
-        "@mui/material",
-        "react-dom",
-        "react",
-      ],
+      external: ["@mui/material", "react"],
       output: {
         globals: {
           react: "React",
         },
+        // Since we publish our ./src folder, there's no point
+        // in bloating sourcemaps with another copy of it.
+        sourcemapExcludeSources: true,
       },
     },
+    sourcemap: true,
+    // Reduce bloat from legacy polyfills.
+    target: "esnext",
+    // Leave minification up to applications.
+    minify: false,
   },
+  plugins: [dts()],
 });
