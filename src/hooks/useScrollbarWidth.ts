0ab61314fd415d55b2f9b7fec131828d20535435
Zach <zmriel@gmail.com>
2022-08-06 18:07:23 -0400

    refactor: useScrollbarWidth to use useElementSize

--- src/hooks/useScrollbarWidth.ts
+++ src/hooks/useScrollbarWidth.ts
@@ -1,13 +1,14 @@
-import { useEventCallback } from "@mui/material";
-import { type MutableRefObject, useState } from "react";
-import useIsoLayoutEffect from "./useIsoLayoutEffect";
+import { type MutableRefObject } from "react";
+import useElementSize from "./useElementSize";
 
 /**
  * Gets the vertical scrollbar width, returning
- * @param element element to check the scrollbar size
+ * @param element partial element containing offset and client widths
  * @returns scrollbar width in pixels, 0 if there is no vertical scrollbar
  */
-function getScrollbarWidth(element: HTMLElement): number {
+function getScrollbarWidth(
+  element: Pick<HTMLElement, "offsetWidth" | "clientWidth">
+): number {
   return element.offsetWidth - element.clientWidth;
 }
 
@@ -17,31 +18,6 @@ function getScrollbarWidth(element: HTMLElement): number {
 export default function useScrollbarWidth<E extends HTMLElement>(
   ref: MutableRefObject<E | null>
 ) {
-  const [width, setWidth] = useState(0);
-
-  const handleResize = useEventCallback(() => {
-    if (!ref.current) {
-      return;
-    }
-
-    const detectedWidth = getScrollbarWidth(ref.current);
-
-    if (width !== detectedWidth) {
-      setWidth(detectedWidth);
-    }
-  });
-
-  useIsoLayoutEffect(() => {
-    if (!ref.current) {
-      return;
-    }
-
-    const observer = new ResizeObserver(() => handleResize);
-    observer.observe(ref.current, { box: "border-box" });
-    handleResize();
-
-    return () => observer.disconnect();
-  }, [handleResize]);
-
-  return width;
+  const size = useElementSize(ref);
+  return getScrollbarWidth(size);
 }
